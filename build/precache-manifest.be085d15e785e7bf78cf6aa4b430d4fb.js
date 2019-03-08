27bc0670447913e44d3e3f84d1d37e426f3df329
Alex_Y <yegupov@gmail.com>
2019-03-08 11:31:07 +0200

    added build

---
+++ build/precache-manifest.be085d15e785e7bf78cf6aa4b430d4fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "55d505a9db3374c4df82",
+    "url": "/static/js/main.99c9d658.chunk.js"
+  },
+  {
+    "revision": "440b2fd9a47d4d541de9",
+    "url": "/static/js/2.e90751cd.chunk.js"
+  },
+  {
+    "revision": "55d505a9db3374c4df82",
+    "url": "/static/css/main.bc14bc82.chunk.css"
+  },
+  {
+    "revision": "9e9947d90e354ef84f40ab52fa19143c",
+    "url": "/index.html"
+  }
+];
